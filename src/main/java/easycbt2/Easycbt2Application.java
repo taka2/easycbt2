c2442caad78974b05b6571cb76c47136f74b7190
taka <takahiro.shigemoto@gmail.com>
2018-03-31 22:53:06 +0900

    Initial Commit.

---
+++ src/main/java/easycbt2/Easycbt2Application.java
@@ -0,0 +1,12 @@
+package easycbt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Easycbt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Easycbt2Application.class, args);
+	}
+}
