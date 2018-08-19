8a3bbad742e40819d5be1158754695c82d46ff91
Takahiro Shigemoto <takahiro.shigemoto@gmail.com>
2018-08-19 10:25:10 +0900

    close #20

--- src/main/java/easycbt2/controller/IndexController.java
+++ src/main/java/easycbt2/controller/IndexController.java
@@ -1,7 +1,10 @@
 package easycbt2.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,7 +15,7 @@ public class IndexController {
 
 	@Autowired
 	UserService userService;
-	
+
 	@RequestMapping("/")
 	public String root() {
 		if(!userService.isExistAdminUser()) {
@@ -27,4 +30,9 @@ public class IndexController {
 		return "error";
 	}
 
+	@GetMapping("/home")
+	public String home(HttpSession httpSession) {
+		httpSession.setAttribute("user", userService.getLoginUser());
+		return "redirect:/examinations";
+	}
 }
